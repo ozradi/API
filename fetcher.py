1a05a2c2b6a93d7c7163bd031f94b6f7f24ef5fe
Oz Radiano <oz@permit.io>
2022-03-11 17:12:11 -1000

    verbosing

--- fetcher.py
+++ fetcher.py
@@ -11,18 +11,20 @@ JSON_PREFIX = "articles"
 if __name__ == "__main__":
     reader = HackerNewsReader()
 
-    # for debug purposes, loading all stories from a mock, instead of from Hackernews
+    logger.debug("Fetching articles")
+
     if DEBUG == 0:
+        # for debug purposes, loading all stories from a mock, instead of from Hackernews
         with open('example/mock_input.json') as json_file:
             all_stories = json.load(json_file)
     else:
         all_stories = reader.getStories(MAX_STORIES)
+        # wrapping stories with json prefix and converting the return value to json
         all_stories = "{\"" + JSON_PREFIX + "\": " + json.dumps(all_stories) + "}"
 
-    logger.debug("Done loading, now filtering")
+    logger.debug("Done fetching, now filtering based on OPA policy")
     filtered_stories = reader.filterStories(all_stories)
-
-    logger.debug(filtered_stories)
+    logger.debug("Done filtering, now printing filtered stories:")
 
     all_stories_as_json = json.loads(all_stories)
     for item in filtered_stories:
